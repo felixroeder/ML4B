32d6214a94bc1e0cdd22ac00525a9e5c8adf148b
felixroeder <167775998+felixroeder@users.noreply.github.com>
2024-05-28 20:31:57 +0200

    Update CodeApp.py

--- CodeApp.py
+++ CodeApp.py
@@ -1,26 +1,26 @@
 import streamlit as st
 import datetime
 from datetime import date
+from  Zwischenabgabe import Code
+
 
 st.title("Stock Prediction App")
 st.subheader('This app is created to forecast the stock market movement using daily news headlines')
 st.image("https://t4.ftcdn.net/jpg/02/20/32/75/240_F_220327557_gRDTuYL4iVG0lWrjgjrv1chBCUunjKlG.jpg")
 
-st.sidebar.header('Please select the parameters from below')
+st.session_state.News = ""
+st.session_state.Price = ""
+st.session_state.result = ""
 
-start_date = st.sidebar.date_input('Start date', date(2008, 1, 1))
-ent_date = st.sidebar.date_input('End date', date(2016, 12, 31))
+with st.form(key="News and stock data"):
+    news_headline = st.text_input("Enter News Headline(s)")
+    stockprice_yesterday = st.text_input("Yesterdays closing Stock Price (currently Google only)")
+    submitted = st.form_submit_button("Submit")
 
-lables = ("1", "0")
-selected_lables = st.selectbox("please select a lable for prediction", lables)
-
-news_headline = st.text_input("Enter News Headline")
+if submitted:
+    st.session_state.News = news_headline
+    st.session_state.Price = stockprice_yesterday
+    st.session_state.result = Code.create_new_price(news_headline, stockprice_yesterday)
+    st.write(st.session_state.result)
 
 data_load_state = st.text("Load data...")
-
-# Data Visualization
-# st.header('Data Visualazation')
-# st.subheader('Plot of the data')
-# st.write("**Note:** Select the date range on the sidebar, or zoom in the plot and select your specific column")
-# fig = px.line(data, x = 'Date', y = data.columns, title = 'Closing price of the stock', width = 1000, height = 600)
-# st.plotly_chart(fig)
