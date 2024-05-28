0ad795f2de176f2b5776ab1e94a5c122fdd55f0f
felixroeder <167775998+felixroeder@users.noreply.github.com>
2024-05-28 19:56:21 +0000

    commit

--- App/CodeApp.py
+++ App/CodeApp.py
@@ -20,9 +20,8 @@ with st.form(key="News and stock data"):
 if submitted:
     st.session_state.News = news_headline
     st.session_state.Price = stockprice_yesterday
+    st.write("Hallo, hier sollte jetzt der Preis für den morgigen Kurs stehen. Leider ist das Modell noch nicht fertig :(")
 
 
-def output(preis):
-    st.write(preis)
 
 data_load_state = st.text("Load data...")
