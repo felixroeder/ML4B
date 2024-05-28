dfc5c33390064ca8416ce469919a6efd4922182b
felixroeder <167775998+felixroeder@users.noreply.github.com>
2024-05-28 20:19:34 +0000

    commit# Please enter the commit message for your changes. Lines starting

--- App/CodeApp.py
+++ App/CodeApp.py
@@ -37,6 +37,7 @@ if st.session_state.submitted:
     with st.form(key='my_form1'):
         Ranking = st.number_input(label='Wie gerne investieren Sie am Aktienmarkt?', max_value = 10, min_value = 1)
         submit_button = st.form_submit_button(label='Submit')
+    if submit_button:
         st.session_state.submitted = True
 
     if st.session_state.submitted:
