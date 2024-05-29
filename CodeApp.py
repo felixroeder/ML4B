9765338055a67e4c6833a925947e502bf74b0ae2
felixroeder <167775998+felixroeder@users.noreply.github.com>
2024-05-29 18:34:00 +0000

    commit

--- CodeApp.py
+++ CodeApp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import datetime
 from datetime import date
 
-#from folder1.QuellCode import create_new_price
+from folder1.QuellCode import create_new_price
 
 # Titel der App
 st.title("Stock Prediction App")
